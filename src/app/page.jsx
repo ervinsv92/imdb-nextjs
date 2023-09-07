3519ffe2ad6b69d970210d50fa34956e7be30521
Ervin Solano Vargas <esolanov@grupocolono.com>
2023-09-07 10:48:36 -0600

    fetch data

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,23 @@
-import Image from 'next/image'
+import Results from "@/components/Results";
 
-export default function Home() {
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({searchParams}) {
+
+  //it read the params url in server side
+  const genre = searchParams.genre || 'fetchTrending';
+  const res = await fetch(`https://api.themoviedb.org/3/${genre === 'fetchTopRated'?'movie/top_rated':'trending/all/week'}?api_key=${API_KEY}&language=en-US&page=1`, {next:{revalidate:10000}});
+  const data = await res.json();
+  const results = data.results;
+  console.log(results)
+
+  if(!res.ok){
+    throw new Error("Fail to fetch data");
+  }
+
   return (
-    <h1 className='text-red-400'>Home</h1>
+    <div>
+      <Results results={results}/>
+    </div>
   )
 }
